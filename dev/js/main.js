4223a93db4468be57a779435025db36569e4b15c
Magomed Kandaev <muhammad-114@mail.ru>
2017-09-16 12:33:59 +0300

    add warning popup

--- dev/js/main.js
+++ dev/js/main.js
@@ -1,23 +1,46 @@
 var loginForm = document.forms.loginForm;
+var warnPopup = document.querySelector('.warn-popup');
+var body = document.body;
+var topDocumentScroll;
+
+var hideWarnMessage = function(e) {
+    if (e.target.classList.contains('warn-popup')) {
+        return
+    }
+    warnPopup.classList.add('hidden');
+    body.classList.remove('frozen');
+    body.querySelector('.center-block').style.marginTop = '';
+    window.scroll(0, topDocumentScroll);
+    document.removeEventListener('click', hideWarnMessage)
+}
+
+var showWarnMessage = function() {
+    warnPopup.classList.remove('hidden');
+    topDocumentScroll = window.scrollY;
+    body.classList.add('frozen');
+    body.querySelector('.center-block').style.marginTop = -topDocumentScroll + 'px';
+    document.addEventListener('click', hideWarnMessage)
+}
 
 loginForm.addEventListener('submit', function(e) {
+    e.preventDefault();
+    e.stopPropagation();
     var formData = new FormData(loginForm);
     var name = formData.get("name");
     var pass = formData.get("password");
     var saveData = formData.get("save");
     if (name && pass) {
         if (Cookies.get(name) !== undefined) {
-            alert('Пользователь с таким именем уже залогинен');
             e.preventDefault()
         } else {
-            alert('Пользователя с таким логином не существует');
+            e.preventDefault();
+            showWarnMessage();
+            // alert('Пользователя с таким логином не существует');
             if (saveData) {
                 Cookies.set(name, pass);
             }
-            e.preventDefault()
         }
     } else {
-        alert('Данные введены не полностью');
         e.preventDefault();
     }
 })
